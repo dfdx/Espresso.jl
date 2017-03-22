f76d650e2e65feebe275af2392819a6851d4171a
Andrei Zhabinski <faithlessfriend@gmail.com>
2017-03-22 09:33:40 +0300

    2

--- src/expand_deps.jl
+++ src/expand_deps.jl
@@ -1,30 +1,9 @@
 
 # expand_deps.jl - collect & expand all dependencies of a variable in  ExGraph
 
-function expand_deps!(g::ExGraph, nd::ExNode{:input}, depth::Int, result::Vector{Expr})
-    # do nothing
-end
+# TODO: do we really need both - collect_deps and expand_deps?
 
-function expand_deps!(g::ExGraph, nd::ExNode{:constat}, depth::Int, result::Vector{Expr})
-    push!(result, to_expr(nd))
-end
-
-function expand_deps!(g::ExGraph, nd::ExNode{:(=)}, depth::Int, result::Vector{Expr})
-    if depth > 0
-        expand_deps!(g, [g[var] for var in dependencies(nd)], depth - 1, result)
-        push!(result, to_expr(nd))
-    end
-end
-
-function expand_deps!(g::ExGraph, nd::ExNode{:call}, depth::Int, result::Vector{Expr})
-    if depth > 0
-        for dep in dependencies(nd)
-            expand_deps!(g, g[end], depth - 1, result)
-        end
-        push!(result, to_expr(nd))
-    end
-end
-
+## collect_deps
 
 function collect_deps!(result::Set{Symbol}, g::ExGraph, nd::ExNode, depth::Int=typemax(Int))
     if depth > 0
@@ -39,12 +18,25 @@ end
 
 
 function collect_deps!(result::Set{Symbol}, g::ExGraph, ex::Expr, depth::Int=typemax(Int))
-    if ex.head == :call
-        for arg in ex.args[2:end]
-            collect_deps!(result, g, arg, depth)
+    if depth > 0
+        if ex.head == :call
+            for arg in ex.args[2:end]
+                if isa(arg, Symbol)
+                    push!(result, arg)
+                end
+                collect_deps!(result, g, arg, depth - 1)
+            end
+        elseif ex.head == :ref
+            push!(result, ex.args[1])
+            collect_deps!(result, g, ex.args[1], depth - 1)
         end
-    elseif ex.head == :ref
-        collect_deps!(result, g, ex.args[1], depth)
+    end
+end
+
+
+function collect_deps!(result::Set{Symbol}, g::ExGraph, x::Symbol, depth::Int=typemax(Int))
+    if haskey(g, x)
+        collect_deps!(result, g, g[x], depth)
     end
 end
 
@@ -68,9 +60,43 @@ function collect_deps(g::ExGraph, ex::Expr, depth::Int=typemax(Int))
 end
 
 
+function collect_deps(g::ExGraph, x::Symbol, depth::Int=typemax(Int))
+    result = Set{Symbol}()
+    collect_deps!(result, g, x, depth)
+    return result
+end
+
+
 collect_deps(g::ExGraph, x, depth::Int=typemax(Int)) = Set{Symbol}()
 
 
+
+## expand_deps
+
+
+function expand_deps!(g::ExGraph, nd::ExNode{:input}, depth::Int, result::Vector{Expr})
+    # do nothing
+end
+
+function expand_deps!(g::ExGraph, nd::ExNode{:constat}, depth::Int, result::Vector{Expr})
+    push!(result, to_expr(nd))
+end
+
+function expand_deps!(g::ExGraph, nd::ExNode{:(=)}, depth::Int, result::Vector{Expr})
+    if depth > 0
+        expand_deps!(g, [g[var] for var in dependencies(nd)], depth - 1, result)
+        push!(result, to_expr(nd))
+    end
+end
+
+function expand_deps!(g::ExGraph, nd::ExNode{:call}, depth::Int, result::Vector{Expr})
+    if depth > 0
+        for dep in dependencies(nd)
+            expand_deps!(g, g[end], depth - 1, result)
+        end
+        push!(result, to_expr(nd))
+    end
+end
 
 function expand_deps(g::ExGraph, nd::ExNode, depth::Int=typemax(Int))
     deps = collect_deps(g, nd, depth)
