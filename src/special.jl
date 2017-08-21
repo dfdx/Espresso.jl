78a9590275d79052c726637ad95b1b47b4c9d0f1
dfdx <faithlessfriend@gmail.com>
2017-08-22 01:44:28 +0300

    canonical now doesn't qualify functions with Main module

--- src/special.jl
+++ src/special.jl
@@ -1,10 +1,8 @@
 
-const SPECIAL_FUNCS = Set{Any}([
-    :(Main.foo),
-    :(Main.foo_grad)
-])
+const SPECIAL_FUNCS = Set{Any}()
 
 
+isspecial(op) = op in SPECIAL_FUNCS
 
 is_special_expr(ex::Expr) = any(idxs == [:(:)] for idxs in find_indices(ex))
 is_special_expr(x) = false
