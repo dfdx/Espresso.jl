5b3bc336aec411f3db214f686081750d278730c5
dfdx <faithlessfriend@gmail.com>
2018-01-13 17:58:34 +0300

    constructors for immutables

--- src/helpers.jl
+++ src/helpers.jl
@@ -16,27 +16,45 @@ function struct_like(m)
         return typeof(m)()
     catch e
         if e isa MethodError
-            println("ERROR: Trying to instantiate $(typeof(m)), but it doesn't " *
-                    "have a default constructor")            
+            println("ERROR: Trying to instantiate mutable type $(typeof(m)), " *
+                    "but it doesn't have a default constructor")            
         end
         throw(e)
     end
 end
 
 
-function __construct(mem::Dict, dzdm_v::Symbol, m; fields...)
-    dz!dm = @get_or_create(mem, dzdm_v, struct_like(m))
-    for (f, val) in fields
-        setfield!(dz!dm, f, val)
+function __construct(mem::Dict, dzdm_v::Symbol, m::T; fields...) where T
+    if isimmutable(m)
+        return __construct_immutable(T; fields...)
+    else
+        dz!dm = @get_or_create(mem, dzdm_v, struct_like(m))
+        for (f, val) in fields
+            setfield!(dz!dm, f, val)
+        end
+        return dz!dm
     end
-    return dz!dm
 end
 
 
-function __construct(m; fields...)
-    dz!dm = struct_like(m)
-    for (f, val) in fields
-        setfield!(dz!dm, f, val)
+function __construct(m::T; fields...) where T
+    if isimmutable(m)
+        return __construct_immutable(T; fields...)
+    else
+        dz!dm = struct_like(m)
+        for (f, val) in fields
+            setfield!(dz!dm, f, val)
+        end
+        return dz!dm
     end
-    return dz!dm
+end
+
+
+function __construct_immutable(::Type{T}; fields...) where T
+    f2i = Dict((f, i) for (i, f) in enumerate(fieldnames(T)))
+    vals = Array{Any}(length(f2i))
+    for (f, val) in fields
+        vals[f2i[f]] = val
+    end
+    return T(vals...)
 end
