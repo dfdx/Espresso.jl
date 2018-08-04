9915b94453ab3f0b9f7b7ea597d88706b46f0cff
dfdx <faithlessfriend@gmail.com>
2018-08-05 01:00:52 +0300

    adapting to julia-0.7-rc2

--- src/core.jl
+++ src/core.jl
@@ -6,6 +6,7 @@
 
 # using Sugar
 using LinearAlgebra
+using Statistics
 import Base: CodeInfo, Slot
 
 include("types.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,4 @@ include("rewrite_test.jl")
 include("simplify_test.jl")
 include("exnode_test.jl")
 include("exgraph_test.jl")
-include("tracked_test.jl")
+# include("tracked_test.jl")  -- tracked arrays are discontinued, use Yota.jl instead
